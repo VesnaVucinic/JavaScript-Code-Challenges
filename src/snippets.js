47ab5de45370525d3ee8f416ec39e42fcf4884e9
VesnaVucinic <vesna.vucinic68@gmail.com>
2021-07-17 23:11:17 +0100

    add sumOf, findAndReplace, RGBToHex code snnipets

--- src/snippets.js
+++ src/snippets.js
@@ -18,3 +18,17 @@ let average = averageOf(5, 2, 4, 7); // 4.5
 const reverseString = str => [str].reverse().join("");
 let a = reverseString("Have a nice day!"); // !yad ecin a evaH
 
+// 5. sumOf
+// This one returns the sum of the given numbers.
+const sumOf = (numbers) => numbers.reduce((a, b) => a + b, 0);
+let sum = sumOf(5, -3, 2, 1); // 5
+
+// 6. findAndReplace
+// This snippets find a given word in a string and replace with another one.
+const findAndReplace = (string, wordToFind, wordToReplace) => string.split(wordToFind).join(wordToReplace);
+let result = findAndReplace("I like banana", "banana", "apple"); // I like apple
+
+// 7. RGBToHex
+// This one convert a color in RGB mode to Hex.
+const RGBToHex = (r, g, b) => ((r << 16) + (g << 8) + b).toString(16).padStart(6, "0");
+let hex = RGBToHex(255, 255, 255); // ffffff
